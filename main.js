ad0439e98c7e4fd1e766c964c0a5f0e849fe29c9
sabof <esabof@gmail.com>
2014-05-14 14:26:29 +0100

    More drafts

--- main.js
+++ main.js
@@ -12,45 +12,91 @@ var MapModel = function(domElement, tools) {
 MapModel.prototype = {
   currentShape: null,
   currentTool: null,
+  shapes: null,
 
   setCurrentTool: function(toolName) {
-
+    this.currentTool = this.tools[toolName];
   },
-  setCurrentShape: function(shapeName) {
 
+  setCurrentShape: function(shape) {
+    this.currentShape = shape;
+  },
+
+  removeShape: function(shape) {
+    var index = this.shapes.indexOf(shape);
+    if (index !== -1) {
+      this.shapes.splice(index, 1);
+      this.setCurrentShape(null);
+    }
+  },
+
+  addShape: function(shape) {
+    this.shapes.push(shape);
+    this.setCurrentShape(shape);
   }
 };
 
 //------------------------------------------------------------------------------
 
-function Shape(map, pos) {}
+function Shape(map) {
+  this.markers = [];
+
+  // When called with no arguments, I'm assuming it's to create a prototype
+  if (! arguments.length) {
+    return;
+  }
+  this.map = map;
+  map.addShape(this);
+}
 
 Shape.prototype = {
+  map: null,
+  markers: null,
+
   delete: function() {
     // var index = indexOf()
     // shapes.splice()
     throw new Error("Not implemented");
+    this.markers.forEach(function(it) {
+
+    });
   },
+
   create: function() {
     throw new Error("Not implemented");
   },
-  move: function(lat, lng) {
+
+  move: function(vertex, latLng) {
     throw new Error("Not implemented");
   }
+};
+
+
+//------------------------------------------------------------------------------
+
+function Fence(map, latLng) {
+  this.path = new google.maps.MVCArray();
+}
+Fence.prototype = new Shape();
+
+Fence.prototype.addVertex = function(latLng) {
+  this.path.insertAt(this.path.length, latLng);
 };
 
 //------------------------------------------------------------------------------
 
-function Fence(map, pos) {
+function Square(map, latLng) {
   Shape.apply(this, arguments);
-  this.path = new google.maps.MVCArray();
+  var marker = new google.maps.Marker({
+    position: latLng,
+    map: map,
+    draggable: true
+  });
+  this.markers.push(marker);
 }
 
-Fence.prototype = {
-  addVertex: function(pos) {
-    this.path
-  }
-};
+Square.prototype = new Shape();
+
 
 //------------------------------------------------------------------------------
 
@@ -62,8 +108,34 @@ Tool.prototype = {
   enable: function() {
 
   },
+
   disable: function() {
 
+  },
+
+  markerDragEnd: function(event) {
+
+  },
+
+  mapClick: function(event) {
+
+  },
+
+  markerClick: function(event) {
+
+  }
+};
+
+//------------------------------------------------------------------------------
+
+function CreateSquareTool(map) {
+  Tool.apply(this, arguments);
+}
+
+CreateSquareTool.prototype = {
+  mapClick: function(event) {
+    var square = new Square(this.map, event.latLng);
+    // this.map.addShape(square);
   }
 };
 
@@ -79,14 +151,13 @@ function MapView(
 
 MapView.prototype = {
   setTool: function(toolName) {
-    this.currentTool = this.tools[toolName];
 
   }
 };
 
 //------------------------------------------------------------------------------
 
-var map = new MapView(
+var map = new MapModel(
   document.getElementById('theMap'), {
     'delete': null,
     'fence': null,
